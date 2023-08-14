b444b068417e3e9cb30d2a66e70cf551730127ee
Abhijit Chirde <chirde.abhi@gmail.com>
2023-08-14 17:40:34 +0530

    Setting up base files

---
+++ src/code.ts
@@ -0,0 +1,1 @@
+figma.showUI(__html__, { width: 290, height: 425 });
---
+++ src/ui.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./ui.css";
+
+declare function require(path: string): any;
+
+function App() {
+  return <main></main>;
+}
+
+ReactDOM.createRoot(document.getElementById("react-page")).render(<App />);
