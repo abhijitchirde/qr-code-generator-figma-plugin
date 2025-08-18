e0d3ca3e2880b9188be708c0f3f983057a57a001
Abhijit Chirde <chirde.abhi@gmail.com>
2025-08-18 21:27:49 +0530

    Final changes to x profile

--- src/components/BottomSection.tsx
+++ src/components/BottomSection.tsx
@@ -11,7 +11,7 @@ const BottomSection = () => {
       <div className="credit-container">
         <p className="credit-text">
           <a
-            href="https://x.com/abhcde"
+            href="https://x.com/abhxde"
             target="_blank"
             rel="noopener noreferrer"
           >
