04278f0965e12ccb4537fe8889ae5ad4e7953be1
abhijitchirde <chirde.abhi@gmail.com>
2025-08-27 16:03:01 +0530

    Changing x back to original

--- src/components/BottomSection.tsx
+++ src/components/BottomSection.tsx
@@ -11,7 +11,7 @@ const BottomSection = () => {
       <div className="credit-container">
         <p className="credit-text">
           <a
-            href="https://x.com/abhxde"
+            href="https://x.com/abhichirde"
             target="_blank"
             rel="noopener noreferrer"
           >
